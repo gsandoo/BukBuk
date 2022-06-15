676e4953ed037f12b603c32f1c3919e332e25695
201815069 <70687333+eunchae01@users.noreply.github.com>
2022-06-15 11:51:46 +0900

    리뷰
    리뷰

---
+++ src/main/java/bukbuk/firstpro/model/BukReviewDTO.java
@@ -0,0 +1,15 @@
+package bukbuk.firstpro.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BukReviewDTO {
+    private String review_id;
+    private String book_title;
+    private double review_rate;
+    private String review_title;
+    private String review_context;
+    private String review_date;
+}
